6ec64e2e68bfe252393c41ed87fcbd165482251a
Sergey Vagaytsev <88379648+VagSer@users.noreply.github.com>
2022-11-01 11:45:44 +0300

    Декомпозиция стилей + комментарий

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/Settings.css'
 import '../styles/index.css'
 import '../styles/GameComponent.css'
 import '../styles/WinScreen.css'
+import '../styles/AnswerElement.css'
+import '../styles/ArrayElement.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
